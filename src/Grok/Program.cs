bd30a54575d6ce392215634bba9fb7c3df02c1f3
Daniel Cazzulino <daniel@cazzulino.com>
2026-01-07 14:27:53 -0300

    Add sample CLI using the Grok client
    Showcases running x/web/code execution tools simultaneously.

---
+++ src/Grok/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Text;
+using Grok;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+// Some users reported not getting emoji on Windows from F5 in VS so we force UTF-8 encoding. 
+if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+    Console.InputEncoding = Console.OutputEncoding = Encoding.UTF8;
+
+var host = Host.CreateApplicationBuilder(args);
+host.Logging.ClearProviders();
+
+host.Configuration.AddDotNetConfig();
+host.Configuration.AddUserSecrets<Program>();
+
+host.Services.AddHttpClient();
+host.Services.ConfigureHttpClientDefaults(x =>
+{
+    if (Debugger.IsAttached)
+        x.ConfigureHttpClient(h => h.Timeout = TimeSpan.MaxValue);
+    else
+        x.AddStandardResilienceHandler();
+});
+
+var cts = new CancellationTokenSource();
+Console.CancelKeyPress += (s, e) => cts.Cancel();
+host.Services.AddSingleton(cts);
+host.Services.AddSingleton<IHostedService, Interactive>();
+
+var app = host.Build();
+
+await app.RunAsync(cts.Token);
